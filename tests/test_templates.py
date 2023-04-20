c7728ecb2ff27f43e7c6f0bd281cd22919a81012
Florian Popek <florian.popek@gmail.com>
2023-04-20 11:05:34 +0200

    Cosmetic

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1711,5 +1711,5 @@ class Test_Miscellaneous:
         input    = "{{ var1 }},{{ var2 }},{{ var3 }}[[[{{ var1 }},{{ var2 }},{{ var3 }}]]][[[end]]]"
         expected = "1,2,33[[[{{ var1 }},{{ var2 }},{{ var3 }}]]] 1,2,33 [[[end]]]"
         template = autojinja.JinjaTemplate.from_string(input)
-        result = template.context(var1 = 1, var3 = 3).context(var2 = 2, var3 = 33).render()
+        result   = template.context(var1 = 1, var3 = 3).context(var2 = 2, var3 = 33).render()
         assert result == expected
