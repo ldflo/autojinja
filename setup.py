271cd47f3acc4fc02be0074f2196725ae0ae38c2
Florian Popek <florian.popek@gmail.com>
2024-03-18 01:22:13 +0100

    Updated setup.py with Python 3.12 classifier

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Software Development :: Code Generators",
     ],
     packages=["autojinja"],
